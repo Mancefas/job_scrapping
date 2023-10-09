48f60901546c8eeac383720084a3524ef1468ae7
Mantvydas Liobė <mantvydasliobe@gmail.com>
2023-10-09 11:29:04 +0300

    cv market

---
+++ cvMarket.py
@@ -0,0 +1,30 @@
+import requests
+from bs4 import BeautifulSoup
+
+websiteName = "https://www.cvmarket.lt"
+URL = websiteName + "/darbo-skelbimai?op=search&search%5Bjob_salary%5D=3&ga_track=homepage&search%5Bcategories%5D%5B%5D=8&search%5Bkeyword%5D=&search%5Bexpires_days%5D=&search%5Bjob_lang%5D=&search%5Bsalary%5D="
+page = requests.get(URL)
+
+soup = BeautifulSoup(page.content, "html.parser")
+jobs = soup.findAll("article", {"data-component": "jobad"})
+
+for job in jobs:
+    linkTag = job.find(class_="jobad-url")
+    link = websiteName + \
+        linkTag.get("href") if linkTag.get("href") is not None else None
+
+    title = job.find("h2").text if job.find(
+        "h2") is not None else None
+
+    company = job.find("span", class_="job-company").text if job.find(
+        "span", class_="job-company") is not None else None
+
+    city = job.find("span", class_="location").find("div", class_=False).text if job.find(
+        "span", class_="location") is not None else None
+
+    time_added = job.find("span", class_=False).text if job.find(
+        "span", class_=False) is not None else None
+
+    salaryBlock = job.find("div", class_="salary-block")
+    salary = salaryBlock.find("span").text if job.find(
+        "span") is not None else None
