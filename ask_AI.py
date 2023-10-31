6b0f75dadaea89ca6b4eac4bc22bf26cfc61c549
Mantvydas Liobė <mantvydasliobe@gmail.com>
2023-10-31 19:48:34 +0200

    check for Lithuanian add, return error

--- ask_AI.py
+++ ask_AI.py
@@ -9,15 +9,18 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 
 
 def ask_ai(text):
-
-    completion = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=[
-            {
-                "role": "system",
-                "content": "Find what is needed in add - give ONLY years of experience, coding languages in max 10 words"
-            },
-            {"role": "user", "content": text}
-        ]
-    )
-    return json.dumps(completion.choices[0].message["content"])
+    lt_characters = ["ą", "č", "ę", "ė", "į", "š", "ų", "ū", "ž"]
+    if any(letter in lt_characters for letter in text):
+        return json.dumps("I don't speek that language")
+    else:
+        completion = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {
+                    "role": "system",
+                    "content": "Find what is needed in add - give ONLY years of experience, coding languages in max 10 words"
+                },
+                {"role": "user", "content": text}
+            ]
+        )
+        return json.dumps(completion.choices[0].message["content"])
