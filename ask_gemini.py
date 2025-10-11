25319ec5eb34bd4218aed915abfd6bc0b926fe5a
Mantvydas Liobe <mantvydasliobe@gmail.com>
2025-10-11 16:58:46 +0300

    added gemini new lib

--- ask_gemini.py
+++ ask_gemini.py
@@ -1,17 +1,19 @@
 import os
-import google.generativeai as genai
+from google import genai
+from google.genai import types
 import json
 from dotenv import load_dotenv
 
-
 load_dotenv()
 
-genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 
 
 def ask_gemini(text):
-
-    model = genai.GenerativeModel("gemini-1.5-flash")
-    response = model.generate_content(
-        ["Find what is needed in this job add - write what web projects are mentioned, give years of experience if there is any mentioned of that, then coding languages in 20 words or less. Write this as a simple text not a markdown.", text])
+    prompt = "Find what is needed in this job add - write what web projects are mentioned, give years of experience if there is any mentioned of that, then coding languages in 20 words or less. Write this as a simple text not a markdown."
+    
+    response = client.models.generate_content(
+        model="gemini-2.5-flash", contents=[prompt, text]
+    )
+   
     return json.dumps(response.text)
