f44564cd8b6a00903cf91c491acad7283ebd0da7
Mantvydas Liobe <mantvydasliobe@gmail.com>
2025-10-25 19:55:44 +0300

    fixed url

--- commons.py
+++ commons.py
@@ -1,6 +1,6 @@
 CV_BANKAS_URL = "https://www.cvbankas.lt/?padalinys%5B%5D=76&keyw="
 CV_LT_URL_START = "https://www.cv.lt"
-CV_LT_URL_DEEPER = "/it-telekomunikaciju-darbai"
+CV_LT_URL_DEEPER = "/jobs?search%5Bcategories%5D%5B0%5D=8"
 CV_MARKET_URL_START = "https://www.cvmarket.lt"
 CV_MARKET_URL_DEEPER = "/darbo-skelbimai?op=search&search%5Bjob_salary%5D=3&ga_track=homepage&search%5Bcategories%5D%5B%5D=8&search%5Bkeyword%5D=&search%5Bexpires_days%5D=&search%5Bjob_lang%5D=&search%5Bsalary%5D="
 CV_ONLINE_URL_START = "https://www.cvonline.lt"
